dc4a9bd6c6ae377c07fa05131d5f2de5ae7a2517
Ashish Dawale <43884208+ashishdawale20@users.noreply.github.com>
2023-10-29 18:15:52 +0530

    Update appwriteApi.js

--- src/api/appwriteApi.js
+++ src/api/appwriteApi.js
@@ -2,7 +2,7 @@ import { Client, ID, Databases, Account } from "appwrite";
 
 const client = new Client();
 client
-  .setEndpoint("https://appwrite.vhaash-yacht-booking-test.in/v1")
+  .setEndpoint(yourappwriteURL")
   .setProject("653e4b682fe37ac82948");
 const databases = new Databases(client);
 const account = new Account(client);
